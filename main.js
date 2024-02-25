16889edbf64608bc4c5f3fb6450abed635e7b3f7
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2024-02-25 17:58:56 -0600

    tipos de datos

---
+++ main.js
@@ -0,0 +1,23 @@
+let var1 = 10;
+console.log(var1);
+console.log("typeOf(var1) -> "+typeof(var1));
+
+let var2 = 10.01;
+console.log(var2);
+console.log("typeOf(var2) -> "+typeof(var2));
+
+let var3 = "String";
+console.log(var3);
+console.log("typeOf(var3) -> "+typeof(var3));
+
+let var4 = true;
+console.log(var4);
+console.log("typeOf(var4) -> "+typeof(var4));
+
+let var5 = null;
+console.log(var5);
+console.log("typeOf(var5) -> "+typeof(var5));
+
+let var6 = undefined;
+console.log(var6);
+console.log("typeOf(var6) -> "+typeof(var6));
